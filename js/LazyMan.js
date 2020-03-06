db0cfe6e2fa5247634ce7eb95744ea4a442b2779
cumthyb <cumthyb@163.com>
2020-03-06 18:01:26 +0800

    lazyman

--- js/LazyMan.js
+++ js/LazyMan.js
@@ -18,9 +18,61 @@
 // Eat supper~
 // 以此类推。
 
-function LazyMan(name){
-  this.man={}
+function LazyMan(name) {
 
+  function Man(name) {
+    this.name = name
+    this.queue = []
+    this.sayHi()
+    Promise.resolve().then(() => {
+      this.next()
+    })
+  }
 
-  
+  Man.prototype.next = function () {
+    let cb = this.queue.shift()
+    cb && cb.call(this)
+    return this
+  }
+
+  Man.prototype.sayHi = function () {
+    console.log(`Hi This is ${this.name}`)
+  }
+
+  Man.prototype.sleep = function (time) {
+    this.queue.push(() => {
+      setTimeout(() => {
+        console.log(`Wake up after ${time}`)
+        this.next()
+      }, time * 1000);
+
+    })
+
+    return this
+  }
+
+  Man.prototype.sleepFirst = function (time) {
+
+    this.queue.unshift(() => {
+      setTimeout(() => {
+        console.log(`Wake up after ${time}`)
+        this.next()
+      }, time * 1000);
+    })
+    return this
+  }
+
+  Man.prototype.eat = function (food) {
+
+    this.queue.push(() => {
+      console.log(`eat ${food}`)
+      this.next()
+    })
+    return this
+  }
+
+  return new Man(name)
 }
+
+// LazyMan('Hank').sleep(10).eat('dinner')
+LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(10).eat('junk food');
