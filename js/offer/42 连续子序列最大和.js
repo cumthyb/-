fc533aceb3535ea31376d161f88fa8b7028613b7
cumthyb <cumthyb@163.com>
2020-03-07 09:55:05 +0800

    最大子序列和

--- js/offer/42 连续子序列最大和.js
+++ js/offer/42 连续子序列最大和.js
@@ -1,15 +1,14 @@
 
-var arr = [1, -2, 3, 10, -4, 7, 2, -5]
-
-
 function search(arr) {
   var subArr = [arr[0]] // 当前最大和序列
   var subMaxArr = [arr[0]] // 记录最大和的子序列
   var maxSum = arr[0],
     sum = arr[0];
   for (var i = 0, l = arr.length; i < l; i++) {
+    // 若以当前位置结尾的子序列的其他值的和为负数，则直接舍弃前面的序列，
+    // 因为无论该值怎么样，加上负数后可能会更小
     if (sum < 0) {
-      sum = arr[i]; // 若以当前位置结尾的子序列的其他值的和为负数，则直接舍弃前面的序列，因为无论该值怎么样，加上负数后可能会更小
+      sum = arr[i]; 
       subArr = [arr[i]]
     } else {
       sum += arr[i];
